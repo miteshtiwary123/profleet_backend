2f06f010cf5b0f44a4e2d303463ce820b9749d77
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2026-04-08 22:54:05 +0530

    registration schema updated

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class RegisterSchema(BaseModel):
     email: str
     mobile: str
     password: str
+    name: Optional[str] = None
+    role: Optional[str] = "user"
 
 
 class LoginSchema(BaseModel):
@@ -14,4 +17,3 @@ class LoginSchema(BaseModel):
 class TokenResponse(BaseModel):
     access_token: str
     refresh_token: str
-    
